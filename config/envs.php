bf9e96818b022ab51631a339a18e8ecb480c0bdf
corentinmb <corentin.moreau2@gmail.com>
2014-12-05 01:04:54 +0100

    Rajout corentin dans le env.

--- config/envs.php
+++ config/envs.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-	'dev' => ['server.local', 'MacBook-Pro.local', 'MacBook-Pro-de-Ananas-Mac-7.local'],
+	'dev' => ['server.local', 'MacBook-Pro.local', 'MacBook-Pro-de-Ananas-Mac-7.local','MacCorentinMB.local'],
 	'prod' => ['INFO-TIMIDE']
 ];
